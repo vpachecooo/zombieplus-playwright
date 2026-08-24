d34703850ac856a2d339c2a57f6fa661dae4dd9f
Victor Pacheco <vpachecooo@gmail.com>
2026-08-24 20:56:26 -0300

    implementando validacao de mensagem de boas vindas no login

--- tests/actions/Login.js
+++ tests/actions/Login.js
@@ -6,10 +6,10 @@ export class Login {
         this.page = page
     }
 
-    async do(email, password) {
+    async do(email, password, username) {
         await this.visit()
         await this.submit(email, password)
-        await this.isLoggedIn()
+        await this.isLoggedIn(username)
     }
 
     async visit() {
@@ -29,8 +29,11 @@ export class Login {
         await expect(this.page.locator('span[class$=alert]')).toHaveText(target)
     }
 
-    async isLoggedIn() {
-        await this.page.waitForLoadState('networkidle')
-        await expect(this.page).toHaveURL(/.*admin/)
+    async isLoggedIn(username) {
+        const loggedUser = this.page.locator('.logged-user')
+        await expect(loggedUser).toHaveText(`Olá, ${username}`)
+
+        // await this.page.waitForLoadState('networkidle')
+        // await expect(this.page).toHaveURL(/.*admin/)
     }
 }
