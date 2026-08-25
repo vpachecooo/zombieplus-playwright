e7f7b55f02d3d445b7792810c198ef6bcd2af784
Victor Pacheco <vpachecooo@gmail.com>
2026-08-25 10:35:54 -0300

    implementando cenario de testes nao permitir cadastrar filme em duplicidade

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -3,25 +3,33 @@ const { test } = require('../support')
 const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
 
+test.beforeAll(async () => {
+    await executeSQL('DELETE FROM movies;')
+})
+
 test('deve cadastrar um novo filme', async ({ page }) => {
-
     const movie = data.guerra_mundial_z
-    await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
-
+    // await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
     await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
-    
     await page.movies.create(movie)
-    
     await page.toast.containText('Cadastro realizado com sucesso!')
 })
 
-test('não deve cadastrar quando campos obrigatórios estiverem vazios', async ({ page }) => {
-
+test('não deve cadastrar quando o título já existir', async ({ page }) => {
+    const movie = data.resident_evil_o_hospedeiro
+    // await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
     await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.create(movie)
+    await page.toast.containText('Cadastro realizado com sucesso!')
+    
+    await page.movies.create(movie)
+    await page.toast.containText('Este conteúdo já encontra-se cadastrado no catálogo')
+})
 
+test('não deve cadastrar quando campos obrigatórios estiverem vazios', async ({ page }) => {
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
     await page.movies.goForm()
     await page.movies.submit()
-
     await page.movies.alertHaveText([
         'Por favor, informe o título.',
         'Por favor, informe a sinopse.',
